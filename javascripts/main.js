0b6cf96f40d9946d5df577725a61310e25345678
Arthur Anglade <aesraethr@gmail.com>
2015-11-02 13:46:42 +0100

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,36 @@
-console.log('This would be the main JS file.');
+var count = 1;
+        function getColor()
+        {
+            return Math.round(Math.random() * 9) + '' + Math.round(Math.random() * 9) + Math.round(Math.random() * 9);
+        }
+        function doIt(item)
+        {
+            item.append('<div></div><div></div><div></div><div></div>');
+            count += 4;
+            $('label').html('count: ' + count);
+            item.css('backgroundColor',  'transparent');
+            item.unbind('mouseenter');
+            
+            var divs = item.children('div');
+            divs.each(function(){
+                var div = $(this);
+                var width = parseInt(div.css('width'));
+                div.css('backgroundColor',  '#' + getColor());
+                div.css('borderRadius',  '0px');
+                div.css('borderRadius',  (width / 2) + 'px');
+                
+                if (width >= 4)
+                {
+                    setTimeout
+                    (
+                        jQuery.proxy(function(){div.mouseenter(function(){doIt(div)});}, div),
+                        200
+                    );
+                }
+            })
+        }
+        
+        $(document).ready(function(){
+            $('div').mouseenter(function(){doIt($(this))});
+        })
+
